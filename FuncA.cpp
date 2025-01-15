c8a037cb04f4e94daad7197c2175903017b92957
Mark <kram91717@gmail.com>
2025-01-15 18:27:27 +0100

    Hotfix

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,19 @@
+#include <cmath>
 #include "FuncA.h"
 
+double factorial(int num) {
+	if (num <= 1) return 1;
+	return num * factorial(num - 1);
+}
+
 double FuncA::calculate(double x) {
-	return 0;
+	double result = M_PI / 2;
+	int n = 3;
+	for (int i = 0; i < n; ++i) {
+		double term = (factorial(2 * i) /
+				(pow(4, i) * pow(factorial(i), 2) * (2 * i + 1))) *
+				pow(x, 2 * i + 1);
+		result -= term;
+	}
+	return result;
 };
