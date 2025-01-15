2c788da6a03d98098d0ef342f95e931ca5e59abf
Mark <kram91717@gmail.com>
2025-01-15 17:53:03 +0100

    Base implementation

---
+++ FuncA.h
@@ -0,0 +1,9 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+	public:
+		static double calculate(double x);
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+	double x = 0.5;
+	std::cout << "Result: " << FuncA::calculate(x) << std::endl;
+	return 0;
+};
