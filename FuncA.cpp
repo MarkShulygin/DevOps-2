b125dd4ded31402f7ccf5f218ac07d84593f43d0
qimzlor <50299061+MarkShulygin@users.noreply.github.com>
2025-01-17 20:26:37 +0100

    Bag fix

--- FuncA.cpp
+++ FuncA.cpp
@@ -8,7 +8,7 @@ double factorial(int num) {
 
 double FuncA::calculate() {
 	double result = M_PI / 2;
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < 3; ++i) {
 		double term = (factorial(2*i) /
 				(pow(4,i) * pow(factorial(i), 2) * (2 * i + 1)));
 		result -= term;
