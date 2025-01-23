59d35d15a4d08de96d1ec87928fc6b51cba9ed21
qimzlor <50299061+MarkShulygin@users.noreply.github.com>
2025-01-23 13:31:16 +0100

    Update scriptB.sh

--- scriptB.sh
+++ scriptB.sh
@@ -4,11 +4,11 @@ SERVER_URL="127.0.0.1/compute"
 
 make_request() {
     curl -s -X GET "$SERVER_URL" > /dev/null
-    echo "Request was sent"
+    echo "Sending request to the server"
 }
 
 while true; do
-    RANDOM_DELAY=$((RANDOM % 2 + 3))
+    RANDOM_DELAY=$((RANDOM % 3 + 4))
     make_request &
     sleep $RANDOM_DELAY
 done
